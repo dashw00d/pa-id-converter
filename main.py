e440a1aef0b8652aa089c5e5f019258d2bd27bb3
Ryan Stefan <letmegoblind@gmail.com>
2018-02-03 16:07:19 -0600

    DB Counter Added

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from flask.ext.heroku import Heroku
 # App config.
 DEBUG = True
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://cymghpdiqxpbmg:37ea79f90517787f13d98e62851dfccae5b4b85884ca05bc1a105bf8f53fe72e@ec2-54-83-203-198.compute-1.amazonaws.com:5432/d7ik4t62h1insf'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://#:#@#'
 #heroku = Heroku(app)
 db = SQLAlchemy(app)
 app.config.from_object(__name__)
